1970f0c1d4210495d567c19e811b878d20f53286
n3rdpower <nikkinorton23@hotmail.com>
2014-02-02 11:16:51 -0500

    Update PBJ_Goal4.py

--- PBJ_Goal4.py
+++ PBJ_Goal4.py
@@ -9,11 +9,11 @@ list1 = (peanut_butter, jelly, bread_serving)
 if min(list1)==max(list1) and min(list1)!= 0 and bread_slice%2 == 0:
 	print "It's Peanut Butter Jelly Time! You have enough servings for {0} sandwiches!".format(min(list1))
 
-elif jelly<max(list1) or peanut_butter<max(list1) or bread_serving<max(list1) and bread_slice%2==0 and max(list1)!= 0:
+elif jelly<max(list1) or peanut_butter<max(list1) or bread_serving<max(list1) and max(list1)!= 0:
 	print "you need {0} servings of jelly".format(max(list1)-jelly)
 	print "you need {0} servings of peanut butter".format(max(list1)-peanut_butter)
 	print "you need {0:.1f} servings of bread".format(max(list1)-bread_serving)
 	print "then you will be able to make {0} sandwiches".format(max(list1))
-	
+
 else:
 	print "you have nothing and cannot make a sandwich"
