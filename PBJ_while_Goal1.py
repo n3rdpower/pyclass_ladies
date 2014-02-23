90ae247c61444f20105cf22dae5f953d080c86e5
n3rdpower <nikkinor@gmail.com>
2014-02-23 09:54:06 -0500

    Update PBJ_while_Goal1.py

--- PBJ_while_Goal1.py
+++ PBJ_while_Goal1.py
@@ -10,10 +10,9 @@
 # Making sandwich #2
 # All done; only had enough bread for 2 sandwiches.
 
-
-bread = 4
-peanut_butter = 3
-jelly = 10
+bread = int(raw_input("how many slices of bread do you have?  "))
+peanut_butter = int(raw_input("how many servings of peanut butter do you have?  "))
+jelly = int(raw_input("how many servings of jelly do you have?  "))
 sandwich = []
 
 while bread >=2 and peanut_butter >=1 and jelly >=1:
@@ -25,5 +24,15 @@ while bread >=2 and peanut_butter >=1 and jelly >=1:
 for index, PBJ in enumerate(sandwich):
 	print "Making sandwich #{0}".format(index+1)
 	
-if bread <2 or peanut_butter <1 or jelly <1:
-	print "All done; only had enough bread for {0} sandwiches.".format(len(sandwich)) #Granted, this is lazy code
+if bread <2 and peanut_butter >=1 and jelly >=1:
+	print "All done; only had enough bread for {0} sandwiches.".format(len(sandwich))
+	
+elif bread >=2 and peanut_butter <=0 and jelly >=1:
+	print "All done; only had enough peanut butter for {0} sandwiches.".format(len(sandwich))
+	
+elif bread >=2 and peanut_butter >=1 and jelly <=0:
+	print "All done; only had enough jelly for {0} sandwiches.".format(len(sandwich))
+	
+elif bread<=0 or peanut_butter==0 or jelly==0:
+	print "You only had enough ingredients for {0} sandwiches.".format(len(sandwich))
+	
